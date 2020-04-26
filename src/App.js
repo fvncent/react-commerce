12aa8c1ca0c8e5ec7a96e26635450b20b5c7929e
Vincent <fvince12@gmail.com>
2020-04-26 22:30:36 +0800

    added ecom authentication

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import React from 'react';
+import './App.css'
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/authentication/authentication.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
-function App() {
-  return (
-    <div>
-      <Switch>
-        <Route   exact path="/" component= {Homepage} />
-        <Route   path="/hats" component= {HatsPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user})
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  
+  render() {
+    return (
+      <div>
+      <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route   exact path="/" component= {Homepage} />
+          <Route   path="/shop" component= {ShopPage} />
+          <Route   path="/signin" component= {SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,53 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDR41qlGcmjx21zwLXM6HDXd431Atpl6xs",
+    authDomain: "crwn-clothing-8f986.firebaseapp.com",
+    databaseURL: "https://crwn-clothing-8f986.firebaseio.com",
+    projectId: "crwn-clothin g-8f986",
+    storageBucket: "crwn-clothing-8f986.appspot.com",
+    messagingSenderId: "382007744928",
+    appId: "1:382007744928:web:3c8ca31927e0c2881fecc5",
+    measurementId: "G-D1EDS1DMNN"
+
+}
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`user/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (err) {
+            console.log('error creating user', err.message)
+        }
+    }
+
+    return userRef;
+    // console.log(firestore.doc('users/12'))
+}
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
